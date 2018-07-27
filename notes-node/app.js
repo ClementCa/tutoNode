2fe9cbeac9fea0b71e7bd7b1c7929d4ffec6d83e
ccarretier <clement.carretier@cegedim-activ.com>
2018-07-27 13:38:02 +0200

    npm lodash and nodemon

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,12 +3,21 @@ console.log('Starting app.js');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
+const _ = require('lodash');
 
-var resultNotes = notes.addNote();
+console.log(_.isString(true));
+console.log(_.isString('Clément'));
+
+var filterArray = _.uniq(['Mike',1, 'Clement', 1, 2, 3, 4]);
+console.log(filterArray);
+
+/*var resultNotes = notes.addNote();
 var resultAdd = notes.add(8, 10);
 
 console.log(resultNotes);
-console.log(resultAdd);
+console.log(resultAdd);*/
+
+
 
 /*var user = os.userInfo();
 
