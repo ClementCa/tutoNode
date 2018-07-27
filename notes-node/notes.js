2411a24a7ff90bd969e4bbe88302799d208d07d1
ccarretier <clement.carretier@cegedim-activ.com>
2018-07-27 15:23:40 +0200

    yargs interface

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,10 +1,20 @@
 console.log('Strating notes.js');
 
-module.exports.addNote = () => {
-    console.log('addNote');
+var addNote = (title, body) => {
+    console.log('Adding note', title, body);
     return 'New note';
 };
 
-module.exports.add = (a,b) => {
-    return a + b;
+var getAll = () => {
+ console.log('Getting all notes');
 };
+
+var getNote = (title) => {
+  console.log('Getting note', title);
+};
+
+var removeNote = (title) => {
+    console.log('Removing note', title);
+};
+
+module.exports = {addNote, getAll, getNote, removeNote};
